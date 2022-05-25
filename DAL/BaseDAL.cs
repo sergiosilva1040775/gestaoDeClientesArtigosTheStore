f29c964b2fd0b94915675ac77fe63ffa3b065b61
sergiosilva1040775 <104777051+sergiosilva1040775@users.noreply.github.com>
2022-05-25 18:51:40 +0100

    1900

--- DAL/BaseDAL.cs
+++ DAL/BaseDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,8 @@ using System.Threading.Tasks;
 namespace gestaoDeClientesArtigosTheStore.DAL
 {
     static class BaseDAL
-    { 
-        
+    {
+
 
         public static string connStr { get; } = "datasource=localhost;port=3306;username=root;password=;database=thestorealdiartigosclientes;SslMode=none";
 
